11a979ae70232da48ca69a71e4b1510b29b6ba2f
unknown <rosana@gmail>
2020-12-12 02:19:42 +0100

    Refactor login to include real JWT

--- postgres-express-node/api/controllers/login.controller.js
+++ postgres-express-node/api/controllers/login.controller.js
@@ -1,18 +1,22 @@
+//provjerava da li za dani username i password postoji u bazi i
+// da li je password tocan te daje token
 const winston = require("winston");
-const { loginServiceInstance } = require("../../services");
+const { loginServiceInstance } = require("../../services"); 
 
 const Logger = winston.loggers.get("logger");
 
 exports.login = async (req, res) => {
   const { username, password } = req.body;
   try {
-    const user = await loginServiceInstance.getUser({ username });
+    //ako je sve tocno, vrati usera i token
+    const {user, token} = await loginServiceInstance.login({ 
+      username, 
+      password
+    });
 
-    if (user && user.password === password) {
-      return res.json({ message: "Login successful", token: "auth_token" });
-    }
-
-    return res.status(401).json({ error: "Not authorized" });
+    //vracamo krajnjem korisniku
+    return res.json({user,token}); 
+    
   } catch (err) {
     Logger.error(err);
     return res.status(400).json({ error: { message: err.message } });
--- postgres-express-node/services/login.service.js
+++ postgres-express-node/services/login.service.js
@@ -1,15 +1,60 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+
+//definira se klasa LoginService
 class LoginService {
   constructor({ logger, userModel }) {
     this.userModel = userModel;
     this.logger = logger;
   }
 
-  async getUser(userDTO) {
-    const user = await this.userModel.findOne({
-      where: userDTO,
+  
+  //nadi korisnika koji ima dani username i spremi objekt u userRecord
+  async login({username, password}){
+    const userRecord =await this.userModel.findOne({
+      where: {username},
     });
-    return user;
+
+    //ako navedeni korisnik nije pronaden
+    if(!userRecord){
+      this.logger.error("User not registered"); //biljezenje u logu da je bezuspjesna prijava
+      throw new Error("Authentification failed");
+    }
+
+    //ako korisnik postoji
+    this.logger.info("Checking password"); 
+    if(userRecord.password === password){
+      this.logger.info("Password correct, proceed and generate JWT"); //biljezenje u logu uspjesnu autentifikaciju
+
+      const user ={
+        username: userRecord.username,
+        role: userRecord.role || "guest",
+      };
+
+      //
+      const payload = {
+        ...user,
+        aud: config.jwt.audience || "localhost/api", //za koga je namjenjen token
+        iss: config.jwt.issuer || "localhost@fesb", // tko je izdao token
+
+      };
+      //stvaranje tokena
+      const token = this.generateToken(payload); 
+      return{user,token};
+    }
+
+    //ako lozinka nije tocna
+    throw new Error("Authentification failed");
   }
+
+  //funckoija za generiranje tokena
+  generateToken(payload){
+    //potpisivanje payloada
+    return jwt.sign(payload, config.jwt.secret,{
+      expiresIn: config.jwt.expiresIn,
+    });
+  }
+
 }
 
 module.exports = LoginService;
